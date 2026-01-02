0e9c6dd9f2a5a6aef9a61763f96d4d6df9fd3eeb
Wtada233 <wtada233@outlook.com>
2026-01-02 17:54:53 +0800

    .

--- src/main/java/org/linuxfirmware/consolePlus/I18n.java
+++ src/main/java/org/linuxfirmware/consolePlus/I18n.java
@@ -4,6 +4,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
