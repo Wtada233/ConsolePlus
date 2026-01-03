fc0e4f80f48751e7e67baaa8bf8863fcb54bbefd
Wtada233 <wtada233@outlook.com>
2026-01-03 13:35:06 +0800

    紧急修复Bug

--- src/main/java/org/linuxfirmware/consolePlus/I18n.java
+++ src/main/java/org/linuxfirmware/consolePlus/I18n.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
 
